df57fe1b24d13b1bebad3e2968b97ae68c84768d
Javier Mejia <jfmejia.xpe@gmail.com>
2023-03-04 19:10:13 -0500

    siguiendo

--- producto/urls.py
+++ producto/urls.py
@@ -3,5 +3,7 @@ from producto import views
 urlpatterns = [
     path('listProduct', views.ProductList.as_view() ),
     path('createProduct', views.ProductCreate.as_view() ),
-    path('productDetail/<int:pk>', views.ProductDetail.as_view() )
+    path('productDetail/<int:pk>', views.ProductDetail.as_view() ),
+    path('productView', views.ProductView.as_view() )
+    
 ]
--- producto/views.py
+++ producto/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from rest_framework import generics
+from rest_framework.views import APIView
 from .models import Product 
 from .serializers import ProductSerializer
+from rest_framework.response import Response
 
 # Create your views here.
 
@@ -16,5 +18,13 @@ class ProductCreate(generics.CreateAPIView):
 class ProductDetail(generics.RetrieveAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+
+class ProductView(APIView):
+
+    def get(self, request, *args):
+        data = Product.objects.all()
+        serializer = ProductSerializer(data,many=True)
+        results = serializer.data
+        return Response(results)
 
     
